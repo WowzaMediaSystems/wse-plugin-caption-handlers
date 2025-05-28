6e9d020a3684dd4fd22d00fb3141955965a743fd
Roger Littin <roger@wowza.com>
2025-05-28 15:21:17 +0100

    Fix comments

--- src/main/java/com/wowza/wms/plugin/captions/caption/CaptionHelper.java
+++ src/main/java/com/wowza/wms/plugin/captions/caption/CaptionHelper.java
@@ -3,11 +3,6 @@
  * This code is licensed pursuant to the Wowza Public License version 1.0, available at www.wowza.com/legal.
  */
 
-/*
- * This code and all components (c) Copyright 2006 - 2025, Wowza Media Systems, LLC.  All rights reserved.
- * This code is licensed pursuant to the Wowza Public License version 1.0, available at www.wowza.com/legal.
- */
-
 package com.wowza.wms.plugin.captions.caption;
 
 import com.wowza.util.StringUtils;
